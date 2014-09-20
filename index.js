91399676ca8d7dce073f14410319c6686273aa3f
Kevin Sawicki <kevinsawicki@gmail.com>
2014-09-20 16:15:15 -0700

    Start handling errors

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ var request   = require('request');
 var url       = require('url');
 var XmlStream = require('xml-stream');
 
+var wrapCallback = function(callback) {
+  var callbackCalled = false;
+  return function() {
+    if (!callbackCalled)
+      callback.apply(null, arguments);
+    callbackCalled = true;
+  };
+}
+
 var getLatestFiling = function(id, callback) {
+  callback = wrapCallback(callback);
+
   var requestOptions = {
     uri: 'http://www.sec.gov/cgi-bin/browse-edgar',
     qs: {
@@ -17,10 +28,12 @@ var getLatestFiling = function(id, callback) {
       type: 4
     }
   };
+  var filingRequest = request(requestOptions);
+  filingRequest.on('error', callback);
 
   var filingUrl;
 
-  var parser = new XmlStream(request(requestOptions), 'utf8');
+  var parser = new XmlStream(filingRequest, 'utf8');
   parser.once('text: entry > content > filing-href', function (node) {
     filingUrl = node.$text.trim();
   });
@@ -29,12 +42,22 @@ var getLatestFiling = function(id, callback) {
     var filingDate = Date.parse(node.$text);
     callback(null, filingUrl, filingDate);
   });
+
+  parser.on('end', function() {
+    callback(new Error("No filing URL and date parsed: " + id))
+  });
 };
 
 var getFilingXmlFileUrl = function(filingHref, callback) {
+  callback = wrapCallback(callback);
+
   var folderUrl = url.resolve(filingHref, '.');
   request(folderUrl, function(error, response, body) {
+    if (error) return callback(error);
+
     var fileName = cheerio(body).find('a[href$=".xml"]').first().text();
+    if (!fileName) return callback(new Error("No link to XML file found: " + folderUrl));
+
     var fileUrl = url.resolve(folderUrl, fileName);
     callback(null, fileUrl);
   });
@@ -42,13 +65,18 @@ var getFilingXmlFileUrl = function(filingHref, callback) {
 
 var getFilingHtmlFileUrl = function(filingHref, callback) {
   request(filingHref, function(error, response, body) {
+    if (error) return callback(error);
+
     var htmlName = cheerio(body).find('a[href^="/Archives"]').first().attr('href');
-    var htmlUrl = url.resolve("http://www.sec.gov/", htmlName);
+    if (!htmlName) return callback(new Error("No link to HTML file found: " + filingHref));
+
+    var htmlUrl = url.resolve('http://www.sec.gov/', htmlName);
     callback(null, htmlUrl);
   });
 }
 
 var parseFilingXml = function(xmlFileUrl, callback) {
+  callback = wrapCallback(callback);
   var parser = new XmlStream(request(xmlFileUrl), 'utf8');
 
   var totalShares = 0;
@@ -90,13 +118,23 @@ var parseFilingXml = function(xmlFileUrl, callback) {
   parser.on('end', function () {
     callback(null, totalShares, amountInDollars);
   });
+
+  parser.on('error', callback);
 };
 
 var getLatestTransaction = function(id, callback) {
   getLatestFiling(id, function(error, filingHref, filingDate) {
+    if (error) return callback(error);
+
     getFilingXmlFileUrl(filingHref, function(error, xmlFileUrl) {
+      if (error) return callback(error);
+
       getFilingHtmlFileUrl(filingHref, function(error, htmlFileUrl) {
+        if (error) return callback(error);
+
         parseFilingXml(xmlFileUrl, function (error, shares, dollarAmount) {
+          if (error) return callback(error);
+
           callback(null, htmlFileUrl, shares, dollarAmount, filingDate);
         });
       });
@@ -112,5 +150,7 @@ Object.keys(ceos).forEach(function(id) {
   getLatestTransaction(id, function(error, filingUrl, shares, dollarAmount, filingDate) {
     if (shares > 0 && dollarAmount > 0)
       console.log(generateTweet(ceos[id], filingUrl, shares, dollarAmount, filingDate));
+    else if (error)
+      console.error(error.message || error);
   });
 });
